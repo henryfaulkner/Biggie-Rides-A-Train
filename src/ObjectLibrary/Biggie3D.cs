ce512e40d890415e828a921cb4f784b508b72a09
Henry Faulkner <hfaulkner27@gmail.com>
2024-03-27 07:56:17 -0400

    Day 42 - Biggie 3D

---
+++ src/ObjectLibrary/Biggie3D.cs
@@ -0,0 +1,141 @@
+using Godot;
+using System;
+
+public partial class Biggie3D : CharacterBody3D
+{
+	private static readonly StringName _MOVE_LEFT_INPUT = new StringName("move_left");
+	private static readonly StringName _MOVE_UP_INPUT = new StringName("move_up");
+	private static readonly StringName _MOVE_RIGHT_INPUT = new StringName("move_right");
+	private static readonly StringName _MOVE_DOWN_INPUT = new StringName("move_down");
+	
+	private static readonly int _SPRITE_FRAME_IDLE = 0;
+	private static readonly int _SPRITE_FRAME_WALK1 = 1;
+	private static readonly int _SPRITE_FRAME_WALK2 = 2;
+	private static readonly int _SPRITE_FRAME_WALK1_LEFT = 3;
+	private static readonly int _SPRITE_FRAME_WALK2_LEFT = 4;
+	public static readonly int _SPRITE_FRAME_CHANGE_INTERVAL = 45;
+	public static readonly int _SPRITE_WALK_FRAME_LENGTH = 2;
+	public static readonly float _BIGGIE_SPEED = 400f;
+
+	private Biggie3D _nodeSelf = null;
+	private Node _nodeBiggieSpriteMeshInstance = null;
+
+	private bool _isMoving = false;
+	private bool _canMove = true;
+	private int _frameIncrement = 0;
+
+	public override void _Ready()
+	{
+		GD.Print("Hello World");
+		_nodeSelf = GetNode<Biggie3D>(".");
+		_nodeBiggieSpriteMeshInstance = GetNode("./SpriteMeshInstance");
+
+		// Access the GDScript instance through the Script property
+		//var spriteMeshInstance = _nodeBiggieSprites.GetScript();
+		//AttemptStoredLocationApplication();
+	}
+
+	public override void _Process(double delta)
+	{
+		Movement(delta);
+	}
+	
+	private int ReturnSpriteWalkFrame(int frameIncrement, bool isLeft = false)
+	{
+		var result = (frameIncrement / _SPRITE_FRAME_CHANGE_INTERVAL) % _SPRITE_WALK_FRAME_LENGTH;
+		// Skip Idle sprite
+		result += 1;
+		// Account for sprite flipping
+		if (isLeft) result += 2;
+		return result;
+	}
+
+	private void Movement(double delta)
+	{
+		Vector3 inputDirection = new Vector3();
+
+		if (Input.IsActionPressed(_MOVE_LEFT_INPUT))
+		{
+			inputDirection.X -= 1.0f;
+			_isMoving = true;
+			_frameIncrement += 1;
+			_nodeBiggieSpriteMeshInstance.Call("set_frame", ReturnSpriteWalkFrame(_frameIncrement, true));
+		}
+		else if (Input.IsActionPressed(_MOVE_UP_INPUT))
+		{
+			inputDirection.Z += 1.0f;
+			_isMoving = true;
+			_frameIncrement += 1;
+			_nodeBiggieSpriteMeshInstance.Call("set_frame", ReturnSpriteWalkFrame(_frameIncrement));
+		}
+		else if (Input.IsActionPressed(_MOVE_RIGHT_INPUT))
+		{
+			inputDirection.X += 1.0f;
+			_isMoving = true;
+			_frameIncrement += 1;
+			_nodeBiggieSpriteMeshInstance.Call("set_frame", ReturnSpriteWalkFrame(_frameIncrement));
+		}
+		else if (Input.IsActionPressed(_MOVE_DOWN_INPUT))
+		{
+			inputDirection.Z -= 1.0f;
+			_isMoving = true;
+			_frameIncrement += 1;
+			_nodeBiggieSpriteMeshInstance.Call("set_frame", ReturnSpriteWalkFrame(_frameIncrement));
+		}
+		else
+		{
+			_isMoving = false;
+			_frameIncrement = 0;
+			_nodeBiggieSpriteMeshInstance.Call("set_frame", _SPRITE_FRAME_IDLE);
+		}
+
+		if (inputDirection != Vector3.Zero)
+		{
+			inputDirection = inputDirection.Normalized();
+		}
+
+		Velocity = inputDirection;
+		MoveAndSlide();
+	}
+
+	public void Collide(KinematicCollision2D collision)
+	{
+		if (collision.GetCollider().HasMethod("Hit"))
+		{
+			collision.GetCollider().Call("Hit");
+		}
+	}
+
+	public bool CanMove()
+	{
+		return _canMove;
+	}
+
+	public void CanMove(bool canMove)
+	{
+		_canMove = canMove;
+	}
+
+	private void AttemptStoredLocationApplication()
+	{
+		////GD.Print("AttemptStoredLocationApplication");
+		try
+		{
+			using (var context = new SaveStateContext())
+			{
+				var contextState = context.Load();
+				var storedLocation = contextState.StoredLocation;
+				var rs = new RelocationService();
+				if (storedLocation != null)
+				{
+					////GD.Print($"Biggie applied Position. X: {storedLocation.X}. Y: {storedLocation.Y}");
+					//_nodeSelf.Position = new Vector2(storedLocation.X, storedLocation.Y);
+				}
+			}
+		}
+		catch (Exception exception)
+		{
+			////GD.Print($"AttemptStoredLocationApplication exception: {exception}");
+		}
+	}
+}
--- src/Play.gd
+++ src/Play.gd
@@ -1,11 +1,12 @@
 extends Button
 
-const _OUTSIDE_STATION_SCENE = preload("res://Pages/Levels/OutsideStation/LevelOutsideStation.tscn")
-const _MAIN_STATION_SCENE = preload("res://Pages/Levels/MainStation/LevelMainStation.tscn")
-const _THERAPIST_OFFICE_SCENE = preload("res://Pages/Levels/TherapistOffice/LevelTherapistOffice.tscn")
-const _CLUB_SCENE = preload("res://Pages/Levels/Club/LevelClub.tscn")
+const _OUTSIDE_STATION_SCENE = preload ("res://Pages/Levels/OutsideStation/LevelOutsideStation.tscn")
+const _MAIN_STATION_SCENE = preload ("res://Pages/Levels/MainStation/LevelMainStation.tscn")
+const _THERAPIST_OFFICE_SCENE = preload ("res://Pages/Levels/TherapistOffice/LevelTherapistOffice.tscn")
+const _CLUB_SCENE = preload ("res://Pages/Levels/Club/LevelClub.tscn")
+const _TEST_PERSPECTIVE_SCENE = preload ("res://Pages/Levels/TestPerspective/LevelTestPerspective.tscn")
 
-const _RELOCATION_SERVICE_SCRIPT = preload("res://Core/RelocationService/RelocationService.cs")
+const _RELOCATION_SERVICE_SCRIPT = preload ("res://Core/RelocationService/RelocationService.cs")
 var _relocation_service = null
 
 # Called when the node enters the scene tree for the first time.
@@ -18,9 +19,9 @@ func _process(delta):
 
 func _on_pressed():
 	var scenePlay = decide_scene()
-	get_tree().change_scene_to_packed(scenePlay);	
-	pass;
-
+	scenePlay = _TEST_PERSPECTIVE_SCENE
+	get_tree().change_scene_to_packed(scenePlay);
+	pass ;
 
 func decide_scene():
 	var storedLocationSceneId = _relocation_service.GetStoredLocationSceneId()
@@ -32,9 +33,9 @@ func decide_scene():
 			result = _OUTSIDE_STATION_SCENE
 		2:
 			result = _MAIN_STATION_SCENE
-		3: 
+		3:
 			result = _THERAPIST_OFFICE_SCENE
-		4: 
+		4:
 			result = _CLUB_SCENE
 		_:
 			result = _OUTSIDE_STATION_SCENE
