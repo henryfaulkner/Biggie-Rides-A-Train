e4e19760833cfe11efe9e4628ad120df3c526697
Henry Faulkner <hfaulkner27@gmail.com>
2024-06-13 08:52:00 -0400

    Day 88

--- src/Pages/Levels/3D/TrainStation/MainTerminal/AquariumGiftShop.cs
+++ src/Pages/Levels/3D/TrainStation/MainTerminal/AquariumGiftShop.cs
@@ -3,55 +3,62 @@ using System;
 
 public partial class AquariumGiftShop : Node3D
 {
-    private static readonly int _FPS = 60;
-    private static readonly StringName _INTERACT_INPUT = new StringName("interact");
+	private static readonly int _FPS = 60;
+	private static readonly StringName _INTERACT_INPUT = new StringName("interact");
 
-    private Node3D _nodeAquariumDoor = null;
-    private Area3D _nodeInteractableArea = null;
-    private Node _nodeOutsideGiftShopDoor = null;
+	private Node3D _nodeAquariumDoor = null;
+	private Area3D _nodeInteractableArea = null;
+	private Node _nodeOutsideGiftShopDoor = null;
 
-    private int FrameIndex { get; set; }
+	private int FrameIndex { get; set; }
+	private bool BeginToOpenDoor { get; set; }
 
-    public override void _Ready()
-    {
-        _nodeAquariumDoor = GetNode<Node3D>("./OuterAquarium/AquariumDoor");
-        _nodeOutsideGiftShopDoor = GetNode<Node>("./OuterAquarium/OutsideGiftShopDoor");
-        _nodeInteractableArea = GetNode<Area3D>("./OuterAquarium/AquariumDoor/InteractableArea3D");
-    }
+	public override void _Ready()
+	{
+		_nodeAquariumDoor = GetNode<Node3D>("./OuterAquarium/AquariumDoor");
+		_nodeOutsideGiftShopDoor = GetNode<Node>("./OuterAquarium/OutsideGiftShopDoor");
+		_nodeInteractableArea = GetNode<Area3D>("./OuterAquarium/AquariumDoor/InteractableArea3D");
 
-    public override void _PhysicsProcess(double _delta)
-    {
-        if (Input.IsActionJustPressed(_INTERACT_INPUT)
-            && HelperFunctions.ContainsBiggie(_nodeInteractableArea.GetOverlappingBodies()))
-        {
-            if (ProcessOpenDoor()) NavigateToGiftShop();
-        }
-    }
+		FrameIndex = 0;
+		BeginToOpenDoor = false;
+	}
 
-    //
-    private bool ProcessOpenDoor()
-    {
-        // Negative y rotation to open door
-        if (FrameIndex % 30 == 0)
-        {
-            _nodeAquariumDoor.Rotation = new Vector3(
-                _nodeAquariumDoor.Rotation.X,
-                _nodeAquariumDoor.Rotation.Y - 15,
-                _nodeAquariumDoor.Rotation.Z
-            );
-        }
+	public override void _PhysicsProcess(double _delta)
+	{
+		if (Input.IsActionJustPressed(_INTERACT_INPUT)
+			&& HelperFunctions.ContainsBiggie(_nodeInteractableArea.GetOverlappingBodies()))
+		{
+			BeginToOpenDoor = true;
+		}
 
-        if (FrameIndex % 120 == 0)
-        {
-            NavigateToGiftShop();
-        }
+		if (BeginToOpenDoor && ProcessOpenDoor()) NavigateToGiftShop();
+	}
 
-        FrameIndex += 1;
-        return false;
-    }
+	//
+	private bool ProcessOpenDoor()
+	{
+		// Negative y rotation to open door
+		if (FrameIndex % 7 == 0 && FrameIndex > 0)
+		{
+			_nodeAquariumDoor.Rotation = new Vector3(
+				_nodeAquariumDoor.Rotation.X,
+				_nodeAquariumDoor.Rotation.Y - 0.045f,
+				_nodeAquariumDoor.Rotation.Z
+			);
+		}
 
-    private void NavigateToGiftShop()
-    {
-        _nodeOutsideGiftShopDoor.Call("redirect");
-    }
+		if (FrameIndex % 120 == 0 && FrameIndex > 0)
+		{
+			BeginToOpenDoor = false;
+			NavigateToGiftShop();
+		}
+
+		FrameIndex += 1;
+		return false;
+	}
+
+	private void NavigateToGiftShop()
+	{
+		_nodeOutsideGiftShopDoor.Call("redirect");
+	}
 }
---
+++ src/Pages/Levels/3D/TrainStation/MainTerminal/Teller3D.cs
@@ -0,0 +1,94 @@
+using Godot;
+using System;
+
+public partial class Teller3D : StaticBody3D
+{
+	private static readonly StringName _INTERACT_INPUT = new StringName("interact");
+
+	private Area3D _nodeInteractableArea = null;
+	private TextBox _nodeTextBox = null;
+	private InteractionTextBox _nodeInteractionTextBox = null;
+
+	public override void _Ready()
+	{
+		_nodeInteractableArea = GetNode<Area3D>("./InteractableArea3D");
+		_nodeTextBox = GetNode<TextBox>("/root/SceneMainTerminal/LevelWrapper/TextBoxWrapper/TextBox");
+		_nodeInteractionTextBox = GetNode<InteractionTextBox>("/root/SceneMainTerminal/LevelWrapper/TextBoxWrapper/InteractionTextBox");
+	}
+
+	public override void _Process(double delta)
+	{
+		if (HelperFunctions.ContainsBiggie(_nodeInteractableArea.GetOverlappingBodies())
+			&& Input.IsActionJustPressed(_INTERACT_INPUT))
+		{
+			DisplayDialogue();
+		}
+	}
+
+	private void DisplayDialogue()
+	{
+		if (!_nodeTextBox.CanCreateDialogue()) return;
+		using (var context = new SaveStateService())
+		{
+			var contextState = context.Load();
+			switch (contextState.DialogueStateTeller)
+			{
+				case Enumerations.DialogueStates.Teller.Introduce:
+					_nodeTextBox.AddDialogue("Hi, welcome to the Station's Teller Station. I can take your [wave amp=50 freq=6]train ticket[/wave] if you have one.");
+					_nodeTextBox.AddDialogue("Usually, I sell train ticket but not today. Today's train is SOLD OUT. Apparently, there is some huge show happening at the CATHEDRAL in West Bay.");
+					_nodeTextBox.AddDialogue("Probably another one of the CONDUCTOR'S doing. What a genius.");
+					_nodeTextBox.AddDialogue("I can tell you don't have a [wave amp=50 freq=6]train ticket[/wave] right now. Come back when you have one.");
+					_nodeTextBox.ExecuteDialogueQueue();
+					contextState.DialogueStateTeller = Enumerations.DialogueStates.Teller.AskForTicket;
+					context.Commit(contextState);
+					break;
+				case Enumerations.DialogueStates.Teller.AskForTicket:
+					_nodeTextBox.AddDialogue("You have a [wave amp=50 freq=6]train ticket[/wave] yet?");
+					if (CheckForTicket(contextState))
+					{
+						_nodeTextBox.AddDialogue("It looks like you got your hands on a [wave amp=50 freq=6]train ticket![/wave]");
+						_nodeTextBox.ExecuteDialogueQueue();
+						_nodeInteractionTextBox.StartInteraction("Are you ready to depart?", "Yes", 1);
+						_nodeInteractionTextBox.AddOption("No", 2);
+						_nodeInteractionTextBox.SelectedOptionId += HandleInteraction_Boarding;
+						_nodeInteractionTextBox.Execute();
+					}
+					else
+					{
+						_nodeTextBox.AddDialogue("It seems like you don't. Come back when you have [wave amp=50 freq=6]train ticket[/wave].");
+						_nodeTextBox.ExecuteDialogueQueue();
+					}
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
+	private bool CheckForTicket(SaveStateModel contextState)
+	{
+		return contextState.HasItemTicketPieceOne
+			&& contextState.HasItemTicketPieceTwo
+			&& contextState.HasItemTicketPieceThree
+			&& contextState.HasItemTicketPieceFour
+			&& contextState.HasItemTape;
+	}
+
+	private void HandleInteraction_Boarding(int selectedOptionId)
+	{
+		if (selectedOptionId == 1)
+		{
+			TriggerFinalCutScene();
+		}
+		else
+		{
+			_nodeTextBox.AddDialogue("No problemo. Come back when you're ready to board.");
+			_nodeTextBox.ExecuteDialogueQueue();
+		}
+	}
+
+	private void TriggerFinalCutScene()
+	{
+		return;
+	}
+}
