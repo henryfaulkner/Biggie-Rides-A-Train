77c1c68a669413937fa09feb566b8a51d772613a
Henry Faulkner <hfaulkner27@gmail.com>
2024-07-07 22:06:23 -0400

    Clean up Core folder

---
+++ src/Core/Services/TextBoxService/TextBoxService.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+public partial class TextBoxService : Node
+{
+    public InteractionTextBox InteractionTextBox { get; set; }
+    public TextBox TextBox { get; set; }
+    public HoverTextBox HoverTextBox { get; set; }
+}
